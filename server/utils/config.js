d748c1c16317faf706a0688c891917ec1fe8c025
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-05-10 15:07:19 +0700

    Initial commit

---
+++ server/utils/config.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const PORT = process.env.PORT || 3000
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
+module.exports = { PORT, MONGODB_URI }
